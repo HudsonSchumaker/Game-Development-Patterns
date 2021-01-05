4fcf42d7f800e1436b49fc401365784fd049d649
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-05 13:28:12 +0100

    Create SwordMaster.cs

---
+++ Prototype/SwordMaster.cs
@@ -0,0 +1,14 @@
+// Hudson Schumaker
+
+public class SwordMaster : Enemy
+{
+    public void Attack()
+    {
+        // some code goes here...
+    }
+    
+    public void Smash Attack() 
+    {
+        // some code goes here...
+    }
+}
