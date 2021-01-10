dafe1d84e64502e376037818348c2dd41ee869b7
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-11 00:32:06 +0100

    Create BasicRifle.cs

---
+++ Decorator/BasicRifle.cs
@@ -0,0 +1,11 @@
+// Hudson Schumaker
+
+public class BasicRifle : IRifle
+{
+    private float basicAccurancy = 5.0f
+    
+    public float GetAccurancy()
+    {
+        return basicAccurancy;
+    }
+}
