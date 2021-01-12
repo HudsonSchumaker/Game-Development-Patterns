2e477b44b9266794d1736beffea51d556a3e5bd9
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-12 09:00:37 +0100

    Update CarPartActiveVisitor.cs

--- Visitor/CarPartActiveVisitor.cs
+++ Visitor/CarPartActiveVisitor.cs
@@ -1,1 +1,11 @@
+using UnityEngine;
+
 // Hudson Schumaker
+
+public class CarPartActiveVisitor : ICarPartVisitor
+{
+    public void Visit(Car car)
+    {
+        Debug.Log("Car starting...");
+    }
+}
