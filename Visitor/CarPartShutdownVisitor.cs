0c55f88498c83e2c06bb02db45a0939b4956212a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-13 08:41:30 +0100

    Update CarPartShutdownVisitor.cs

--- Visitor/CarPartShutdownVisitor.cs
+++ Visitor/CarPartShutdownVisitor.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 // Hudson Schumaker
 
 public class CarPartShutdownVisitor : ICarPartVisitor
-{}
+{
+    public void Visi(Car car)
+    {
+        Debug.Log("Car is off.");
+    }
+    
+    
+}
