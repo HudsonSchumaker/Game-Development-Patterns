4da038f1e61e9a95a501d318547bd3297c101d30
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-15 08:23:50 +0100

    Update Car.cs

--- Visitor/Car.cs
+++ Visitor/Car.cs
@@ -3,5 +3,14 @@ using UnityEngine;
 // Hudson Schumaker
 public class Car : ICarPart
 {
-
+    private ICarPart [] carParts;
+    
+    public Car() 
+    {
+        carParts = {};
+    }
+    
+    public void Accept(ICarPartVisitor visitor)
+    {}
+    
 }
