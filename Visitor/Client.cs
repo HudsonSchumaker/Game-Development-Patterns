8138758d1f1952e4b415695b24ce429803ab4850
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-15 08:32:55 +0100

    Update Client.cs

--- Visitor/Client.cs
+++ Visitor/Client.cs
@@ -1,3 +1,20 @@
 using UnityEngine;
 
 // Hudson Schumaker
+public class Client : MonoBehavior
+{
+    void Update()
+    {
+        // Start car
+        if ()
+        {
+        
+        }
+        
+        // Turn off car
+        if ()
+        {
+            
+        }
+    }
+}
