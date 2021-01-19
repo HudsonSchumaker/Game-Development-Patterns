250b9208c9cc32910d82b7a7b6d3145219b31e15
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-19 09:23:14 +0100

    Update Client.cs

--- FactoryMethod/Client.cs
+++ FactoryMethod/Client.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 
 // Hudson Schumaker
-
 public class Client : MonoBehaviour
 {
+    public NPCSpawer spawner;
     
+    public void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            spawer.SpawnVillagers();
+        }
+    }
 }
