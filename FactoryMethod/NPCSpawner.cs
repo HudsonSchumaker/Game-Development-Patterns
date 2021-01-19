ecc729558adc4c1415f5be07017ffeae4219f213
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-19 09:18:21 +0100

    Update NPCSpawner.cs

--- FactoryMethod/NPCSpawner.cs
+++ FactoryMethod/NPCSpawner.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
 // Hudson Schumaker
-
 public NPCSpawner : MonoBehaviour
 {
    private NPC_Factory factory = new NPCFactory();
@@ -9,6 +8,10 @@ public NPCSpawner : MonoBehaviour
    public void SpawnVillagers() 
    {
          INPC farmer = factory.GetNPC(NPCtype.Farmer);
+         INPC beggar = factory.GetNPC(NPCtype.Beggar);
+         INPC shopOwner = factory.GetNPC(NPCtype.Shopowner);
+         
+         farmer.Speak();
          
    }
 }
