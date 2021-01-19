093d23332faecd5fa52c6f8328604ae38990262e
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-19 08:48:16 +0100

    Update NPCFactory.cs

--- FactoryMethod/NPCFactory.cs
+++ FactoryMethod/NPCFactory.cs
@@ -4,5 +4,14 @@ using UntyEngine;
 
 public class NPCFactory : MonoBehavior
 {
-
+    public INPC GetNPC(NPCType type) 
+    {
+        switch(type)
+        {
+            case NPCType.Beggar:
+               NPC beggar = new Beggar();
+            return beggar;
+        }
+    }
+    
 }
