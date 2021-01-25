1403e07f4a6ba4331d0ff5eb53f86bd8a24a127e
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-25 09:02:02 +0100

    Update TurnOffCommand.cs

--- Command/TurnOffCommand.cs
+++ Command/TurnOffCommand.cs
@@ -1,1 +1,11 @@
 // Hudson Schumaker
+
+public class TurnOnCommand
+{
+    public TurnOnCommand(RemoteControlDevice receiver) : base(receiver) {}
+    
+    public override void Execute()
+    {
+        remoteControlDevice.TurnOn();
+    }
+}
