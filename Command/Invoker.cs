5a40de8680efbfdd1d3a22d584a6e51267bdfd6f
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-31 15:31:52 +0100

    Update Invoker.cs

--- Command/Invoker.cs
+++ Command/Invoker.cs
@@ -1,1 +1,15 @@
 // Hudson Schumaker
+public class Invoker
+{
+    private Command command;
+
+    public void SetCommand(Command command)
+    {
+        this.command = command;
+    }
+
+    public void ExecuteCommand()
+    {
+        command.Execute();
+    }
+}
