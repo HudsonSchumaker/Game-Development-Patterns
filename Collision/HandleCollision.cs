a0a67a5d0144718243f78c217c7723aa145f3fc1
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-03-02 20:16:05 +0100

    Update HandleCollision.cs

--- Collision/HandleCollision.cs
+++ Collision/HandleCollision.cs
@@ -5,7 +5,15 @@ public class HandleCollision : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+           
+        }
+
+        if (collision.CompareTag("Shuriken"))
+        {
+           
+        }
     }
     
     void OnTriggerExit2D(Collider2D collision)
