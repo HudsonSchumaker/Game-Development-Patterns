dd4c04dbc0899d49a8b6d730d7c1f1fdea44d94a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-03-04 09:51:44 +0100

    Update HandleCollision.cs

--- Collision/HandleCollision.cs
+++ Collision/HandleCollision.cs
@@ -6,6 +6,22 @@ public class HandleCollision : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // check if is necessarys
+        // check what is necessary
     }
+    
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            transform.parent = collision.gameObject.transform;
+            return;
+        }
+
+        if (collision.gameObject.tag == "Spike")
+        {
+            this.DoDamage();
+        }
+    }
+
+   
 }
