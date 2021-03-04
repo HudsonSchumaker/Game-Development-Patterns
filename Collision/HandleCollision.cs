2f76ce8ba50f9894908f4e1cdffd12d437483fed
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-03-04 09:51:14 +0100

    Update HandleCollision.cs

--- Collision/HandleCollision.cs
+++ Collision/HandleCollision.cs
@@ -3,4 +3,9 @@ using UnityEngine;
 // Hudson Schumaker
 public class HandleCollision : MonoBehaviour 
 {
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // check if is necessarys
+    }
 }
